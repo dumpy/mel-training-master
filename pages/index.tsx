3064d3e4d4ee8f3f6a84ea313479164a062b4717
dumpy <jumpy.dumpy@gmail.com>
2024-08-28 17:29:15 +1000

    add new banner

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,11 @@
-import React, { useState, useEffect } from 'react';
-import { onEntryChange } from '../contentstack-sdk';
-import RenderComponents from '../components/render-components';
-import { getPageRes } from '../helper';
-import Skeleton from 'react-loading-skeleton';
+import React, { useState, useEffect } from "react";
+import { onEntryChange } from "../contentstack-sdk";
+import RenderComponents from "../components/render-components";
+import { getPageRes } from "../helper";
+import Skeleton from "react-loading-skeleton";
 import { Props, Context } from "../typescript/pages";
 
 export default function Home(props: Props) {
-
   const { page, entryUrl } = props;
 
   const [getEntry, setEntry] = useState(page);
@@ -14,7 +13,7 @@ export default function Home(props: Props) {
   async function fetchData() {
     try {
       const entryRes = await getPageRes(entryUrl);
-      if (!entryRes) throw new Error('Status code 404');
+      if (!entryRes) throw new Error("Status code 404");
       setEntry(entryRes);
     } catch (error) {
       console.error(error);
@@ -25,15 +24,21 @@ export default function Home(props: Props) {
     onEntryChange(() => fetchData());
   }, []);
 
-  return getEntry ? (
-    <RenderComponents
-      pageComponents={getEntry.page_components}
-      contentTypeUid='page'
-      entryUid={getEntry.uid}
-      locale={getEntry.locale}
-    />
-  ) : (
-    <Skeleton count={3} height={300} />
+  return (
+    <>
+      <div className="banner">*** Emergency Emergency ***</div>
+      {/* Add the banner here */}
+      {getEntry ? (
+        <RenderComponents
+          pageComponents={getEntry.page_components}
+          contentTypeUid="page"
+          entryUid={getEntry.uid}
+          locale={getEntry.locale}
+        />
+      ) : (
+        <Skeleton count={3} height={300} />
+      )}
+    </>
   );
 }
 
